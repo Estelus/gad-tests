2a3759da84e805ae34ac6d62f0f7f331d98dd927
Maciej Mazoń <maciej.mazon@gmail.com>
2025-04-08 09:30:52 +0200

    feat: dotenv for env var

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  globalSetup: require.resolve('./src/global-setup.ts'),
   timeout: 60_000,
   expect: { timeout: 10_000 },
   fullyParallel: true,
@@ -12,11 +13,11 @@ export default defineConfig({
   workers: undefined,
   reporter: 'html',
   use: {
+    baseURL: process.env.BASE_URL,
     actionTimeout: 0,
-    trace: 'retain-on-failure',
+    trace: 'on',
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    baseURL: 'http://localhost:3000',
   },
 
   projects: [
