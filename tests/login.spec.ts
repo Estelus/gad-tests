0f9c5a85dfa9b8f470637efbcdfb9960e92e74d9
Maciej Mazoń <maciej.mazon@gmail.com>
2025-04-08 10:37:01 +0200

    chore: test data for user email

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,6 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify menu main buttons', () => {
@@ -7,11 +8,11 @@ test.describe('Verify menu main buttons', () => {
     page,
   }) => {
     //Arrange
-    const userEmail = 'Moses.Armstrong@Feest.ca';
+    const userEmail = testUser1.userEmail ?? '';
     const userPassword = 'test1';
     const loginPage = new LoginPage(page);
-    await loginPage.goto();
     // Act
+    await loginPage.goto();
     await loginPage.login(userEmail, userPassword);
     const welcomePage = new WelcomePage(page);
     const title = await welcomePage.title();
