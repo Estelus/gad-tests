0bdd919416190df5b18051b052c1340ea3d959ca
Maciej Mazoń <maciej.mazon@gmail.com>
2025-04-09 09:04:14 +0200

    test: negative for login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,19 +4,36 @@ import { testUser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify menu main buttons', () => {
-  test('comments button navigates to comments page @GAD-R01-03', async ({
-    page,
-  }) => {
+  test('login with correct credentials @GAD-R02-01', async ({ page }) => {
     //Arrange
     const userEmail = testUser1.userEmail;
     const userPassword = testUser1.userPassword;
     const loginPage = new LoginPage(page);
+
     // Act
     await loginPage.goto();
     await loginPage.login(userEmail, userPassword);
     const welcomePage = new WelcomePage(page);
     const title = await welcomePage.title();
+
     // Assert
     expect(title).toContain('Welcome');
+  });
+  test('login with incorrect credentials @GAD-R02-01', async ({ page }) => {
+    //Arrange
+    const userEmail = testUser1.userEmail;
+    const userPassword = 'incorrectPassword';
+    const loginPage = new LoginPage(page);
+
+    // Act
+    await loginPage.goto();
+    await loginPage.login(userEmail, userPassword);
+
+    // Assert
+    await expect(loginPage.loginError).toHaveText(
+      'Invalid username or password',
+    );
+    const title = await loginPage.title();
+    expect(title).toContain('Login');
   });
 });
