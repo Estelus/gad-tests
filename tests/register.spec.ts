13a9b960acd8e30aef5f794360b9ce2ddb509178
Maciej Mazoń <maciej.mazon@gmail.com>
2025-04-13 09:17:31 +0200

    chore: register user model

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,3 +1,4 @@
+import { RegisterUser } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
@@ -10,30 +11,29 @@ test.describe('Verify registration', () => {
     { tag: ['@GAD-R03-01', '@GAD-R03-02', '@GAD-R03-03'] },
     async ({ page }) => {
       //Arrange
-      const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
-      const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
-      const userEmail = faker.internet.email({
-        firstName: userFirstName,
-        lastName: userLastName,
-      });
-      const userPassword = faker.internet.password({
-        length: 10,
-        memorable: true,
-        pattern: /[A-Z]/,
+      const registerUserData: RegisterUser = {
+        userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+        userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+        userEmail: '',
+        userPassword: faker.internet.password({
+          length: 10,
+          memorable: true,
+          pattern: /[A-Z]/,
+        }),
+      };
+
+      registerUserData.userEmail = faker.internet.email({
+        firstName: registerUserData.userFirstName,
+        lastName: registerUserData.userLastName,
       });
 
       const registerPage = new RegisterPage(page);
 
       // Act
       await registerPage.goto();
-      await registerPage.register(
-        userFirstName,
-        userLastName,
-        userEmail,
-        userPassword,
-      );
-
+      await registerPage.register(registerUserData);
       const expectedAlertPopupText = 'User created';
+
       // Assert
       await expect(registerPage.alertPopUp).toHaveText(expectedAlertPopupText);
 
@@ -43,7 +43,10 @@ test.describe('Verify registration', () => {
       expect.soft(title).toContain('Login');
 
       // Assert
-      await loginPage.login(userEmail, userPassword);
+      await loginPage.login(
+        registerUserData.userEmail,
+        registerUserData.userPassword,
+      );
 
       const welcomePage = new WelcomePage(page);
       const titleWelcomePage = await welcomePage.title();
